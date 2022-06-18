7d7ed81d7603eebce11c9f819982cae7aa61209f
Muhiddinov Sherzamon <107454882+Sherzamon0206@users.noreply.github.com>
2022-06-18 11:56:59 +0500

    sherzamon

--- bot/management/commands/bot.py
+++ bot/management/commands/bot.py
@@ -38,7 +38,7 @@ from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKey
 # else:
 #     TOKEN=settings.Token
 #
-
+button = ReplyKeyboardMarkup([["GetMyID"]], resize_keyboard=True)
 def start(update: Update, context: CallbackContext):
     id = update.effective_user.id
     f_name = update.effective_user.first_name
@@ -54,24 +54,48 @@ def start(update: Update, context: CallbackContext):
     except:
 
         user, created = Profile.objects.get_or_create(exeterenal_id=id, username=username, f_name=f_name, l_name=l_name)
-    update.message.reply_text(f"your ID:  {update._effective_user.id}\ncurrent chat ID:  {update.message.chat_id}")
+    update.message.reply_text(f"your ID:  {update._effective_user.id}\ncurrent chat ID:  {update.message.chat_id}",reply_markup=button)
     return 'bot'
 
 
+def users(update:Update,context:CallbackContext):
+    count=Profile.objects.all().count()
+    update.message.reply_text(f"foydalanuvchilar soni : {count}",reply_markup=button)
+    return 'bot'
+def users2(update:Update,context:CallbackContext):
+    users=Profile.objects.all()
+    update.message.reply_text(f"foydalanuvchilar soni : {users.count()}",reply_markup=button)
+    for i in users:
+        try:
+            update.message.reply_text(f"""{i.f_name} | {i.l_name} | {i.exeterenal_id} | {i.username}""")
+
+
+        except:
+            continue
+
+    return 'bot'
+
 def text(update: Update, context: CallbackContext):
-    update.message.reply_text(f"reply: {update.message.text}")
+    update.message.reply_text(f"reply: {update.message.text}",reply_markup=button)
 
     return 'bot'
 
 
 conv_handler = ConversationHandler(
-    entry_points=[CommandHandler('start', start)],
+    entry_points=[CommandHandler('start', start),
+                MessageHandler(Filters.regex('^(' + 'GetMyID' + ')$'), start),
+                  ],
     states={
         'bot': [
-            MessageHandler(Filters.all, start)
+
+            MessageHandler(Filters.regex('^(' + 'sherzamon1' + ')$'), users),
+            MessageHandler(Filters.regex('^(' + 'sherzamon2' + ')$'), users2),
+            MessageHandler(Filters.regex('^(' + 'GetMyID' + ')$'), start),
+            MessageHandler(Filters.all, start),
         ],
     },
 fallbacks = [
+MessageHandler(Filters.regex('^(' + 'GetMyID' + ')$'), start),
     CommandHandler('start', start)
 ]
 
